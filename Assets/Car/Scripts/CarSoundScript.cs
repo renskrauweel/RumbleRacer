9ff045442b7bf98ddce15409f30e75b7f9ae3677
Rens Krauweel <info@rens.frl>
2020-05-04 17:46:28 +0200

    Sound effects and background music

---
+++ Assets/Car/Scripts/CarSoundScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CarSoundScript : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    public AudioClip CollisionClip;
+    public AudioClip CheckpointClip;
+    public float maxEnginePitch = 2;
+    public float minEnginePitch = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _audioSource.pitch += .01f;
+            if (_audioSource.pitch > maxEnginePitch) _audioSource.pitch = maxEnginePitch;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _audioSource.pitch -= .01f;
+            if (_audioSource.pitch < minEnginePitch) _audioSource.pitch = minEnginePitch;
+        }
+    }
+
+    private void OnCollisionEnter()
+    {
+        _audioSource.PlayOneShot(CollisionClip);
+    }
+}
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using Lib.Services;
 using UnityEngine;
+using Random = System.Random;
 
 public class GameManager : MonoBehaviour
 {
     public bool StartAiStream = false;
+    private AudioSource _audioSource;
+    public AudioClip BackgroundMusic1;
+    public AudioClip BackgroundMusic2;
+    public AudioClip BackgroundMusic3;
     
     // Start is called before the first frame update
     void Start()
     {
         if (StartAiStream) DoStartAiStream();
+        _audioSource = GetComponent<AudioSource>();
+        
+        setRandomBackgroundMusic();
     }
 
     // Update is called once per frame
@@ -28,6 +36,23 @@ public class GameManager : MonoBehaviour
         catch (Exception e)
         {
             Debug.Log(e);
+        }
+    }
+
+    private void setRandomBackgroundMusic()
+    {
+        Random r = new Random();
+        switch (r.Next(3))
+        {
+            case 0: 
+                _audioSource.PlayOneShot(BackgroundMusic1, .25f);
+                break;
+            case 1: 
+                _audioSource.PlayOneShot(BackgroundMusic2, .25f);
+                break;
+            case 2: 
+                _audioSource.PlayOneShot(BackgroundMusic3, .25f);
+                break;
         }
     }
 }
