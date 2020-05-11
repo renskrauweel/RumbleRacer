ba541de6c958beeb4efe0d8641d4b4bf49114d27
Rens Krauweel <info@rens.frl>
2020-05-11 13:52:48 +0200

    Prefab fixes + updated scenes

--- Assets/Car/Scripts/CarSoundScript.cs
+++ Assets/Car/Scripts/CarSoundScript.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CarSoundScript : MonoBehaviour
 {
@@ -29,5 +30,10 @@ public class CarSoundScript : MonoBehaviour
     private void OnCollisionEnter()
     {
         _audioSource.PlayOneShot(CollisionClip);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Checkpoint")) _audioSource.PlayOneShot(CheckpointClip);
     }
 }
