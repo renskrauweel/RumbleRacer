2b00d388aedd89d767c878d4f50ade0c41ab9caf
Rens Krauweel <info@rens.frl>
2020-05-25 16:57:36 +0200

    Save laptime in highscores file on PersistentDataPath

--- Assets/StartFinishLine/FinishLineScript.cs
+++ Assets/StartFinishLine/FinishLineScript.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class FinishLineScript : MonoBehaviour
 {
     private CarRaceTimeScript carRaceTimeScript;
-    
+    private string highscoreFileDestination;
+    private string playername;
+
+    private void Start()
+    {
+        highscoreFileDestination = Application.persistentDataPath + "/highscores.dat";
+        playername = PlayerPrefs.GetString("playername");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         carRaceTimeScript = other.gameObject.GetComponentInParent<CarRaceTimeScript>();
@@ -15,10 +25,27 @@ public class FinishLineScript : MonoBehaviour
         {
             carRaceTimeScript.SetCurrentRaceTime();
             carRaceTimeScript.SetHitFinishLine(true);
-            Debug.Log("Finished in "+carRaceTimeScript.GetCurrentRaceTimeMs()+"MS");
+            float endTime = carRaceTimeScript.GetCurrentRaceTimeMs();
+            SaveHighscore(endTime);
+            Debug.Log("Finished in "+endTime+"MS");
         } else if (!carRaceTimeScript.HasHitAllCheckPoints())
         {
             Debug.Log("You haven't hit all checkpoints!");
         }
+    }
+
+    private void SaveHighscore(float endTime)
+    {
+        if (playername.Length == 0) return;
+        
+        // Open file
+        FileStream file = File.Exists(highscoreFileDestination) ? File.OpenWrite(highscoreFileDestination) : File.Create(highscoreFileDestination);
+
+        // Write
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, playername + "-" + endTime +"MS");
+        
+        // Close
+        file.Close();
     }
 }
