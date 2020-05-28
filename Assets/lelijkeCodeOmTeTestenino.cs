280f229b59050160ed789605f4b3897522ee9227
kevinhaakma <kevinhaakma@zoho.com>
2020-05-28 19:57:42 +0200

    No Comment

---
+++ Assets/lelijkeCodeOmTeTestenino.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lelijkeCodeOmTeTesten : MonoBehaviour
+{
+    public static int DitIsMetEenHoofdletter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void dubbelecode1()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(lengte);
+            Debug.Log(lengte);
+            Debug.Log(lengte);
+            Debug.Log(lengte);
+            Debug.Log(lengte);
+        }
+    }
+    void dubbelecode2()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(lengte);
+            Debug.Log(lengte);
+            Debug.Log(lengte);
+            Debug.Log(lengte);
+            Debug.Log(lengte);
+        }
+    }
+}
